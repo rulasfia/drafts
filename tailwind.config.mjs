429f61b6cf3a7f7461ce904a5a53de782073eedf
M Khoirul Asfian <mk.asfian@gmail.com>
2024-08-20 22:37:32 +0700

    fix: fix large bundle size from shiki

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,6 @@
 import typography from "@tailwindcss/typography";
 import defaultTheme from "tailwindcss/defaultTheme";
-import color from "tailwindcss/colors";
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
