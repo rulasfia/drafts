9e33927c6f313b77aca544baa78597b12e2cde9d
M Khoirul Asfian <mk.asfian@gmail.com>
2024-08-20 21:59:53 +0700

    feat: suppport auto dark mode

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,6 @@
 import typography from "@tailwindcss/typography";
 import defaultTheme from "tailwindcss/defaultTheme";
-
+import color from "tailwindcss/colors";
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -9,6 +9,12 @@ export default {
 			fontFamily: {
 				sans: ["Geist", ...defaultTheme.fontFamily.sans],
 				mono: ["Geist Mono", ...defaultTheme.fontFamily.mono],
+			},
+			colors: {
+				transparent: "transparent",
+				current: "currentColor",
+				gray: color.neutral,
+				...color,
 			},
 		},
 	},
