f271a55d020a8d0c687b3cbc70a0f8a197bd5d41
M Khoirul Asfian <mk.asfian@gmail.com>
2024-08-28 10:53:41 +0700

    style: change zinc to neutral & wavy underline

--- src/components/publish-content-switch.svelte
+++ src/components/publish-content-switch.svelte
@@ -30,7 +30,7 @@
 	type="button"
 	on:click={changeContentStatusHandler}
 	data-status={localStatus}
-	class="bg-gradient-to-b font-semibold text-sm px-8 h-8 w-full max-w-32 inline-flex justify-center items-center rounded-lg ring-1 hover:ring-2 shadow hover:shadow-md transition-all duration-150 ease-out dark:text-neutral-800 data-[status=private]:from-orange-100 data-[status=private]:to-orange-300 data-[status=private]:dark:from-orange-300 data-[status=private]:dark:to-orange-600 data-[status=private]:ring-orange-200 data-[status=private]:dark:ring-orange-500 data-[status=private]:shadow-orange-400/50 data-[status=public]:from-zinc-100 data-[status=public]:to-zinc-300 data-[status=public]:dark:from-zinc-300 data-[status=public]:dark:to-zinc-600 data-[status=public]:dark:ring-zinc-500 data-[status=public]:ring-zinc-200 data-[status=public]:shadow-zinc-400/50"
+	class="bg-gradient-to-b font-semibold text-sm px-8 h-8 w-full max-w-32 inline-flex justify-center items-center rounded-lg ring-1 hover:ring-2 shadow hover:shadow-md transition-all duration-150 ease-out dark:text-neutral-800 data-[status=private]:from-orange-100 data-[status=private]:to-orange-300 data-[status=private]:dark:from-orange-300 data-[status=private]:dark:to-orange-600 data-[status=private]:ring-orange-200 data-[status=private]:dark:ring-orange-500 data-[status=private]:shadow-orange-400/50 data-[status=public]:from-neutral-100 data-[status=public]:to-neutral-300 data-[status=public]:dark:from-neutral-300 data-[status=public]:dark:to-neutral-600 data-[status=public]:dark:ring-neutral-500 data-[status=public]:ring-neutral-200 data-[status=public]:shadow-neutral-400/50"
 >
 	<!-- {status === "private" ? "Publish" : "Unpublish"} -->
 	{#if isUpdating}
