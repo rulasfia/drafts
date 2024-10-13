6df27e6d72a1af44c8f8a861f28e0a189aa2684c
M Khoirul Asfian <mk.asfian@gmail.com>
2024-10-13 19:18:08 +0700

    feat: add tags properties

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,6 +4,7 @@ const drafts = defineCollection({
 	schema: z.object({
 		title: z.string(),
 		description: z.string().optional(),
+		tags: z.string().array().optional(),
 		published: z.boolean(),
 		publishedAt: z.date(),
 	}),
